2407686255aaf2f40dd6240ab7f41f14a91df613
vlw0052 <leon@vlw2.com>
2017-05-11 13:25:15 -0500

    Multiplication Added && Code Clean Up

--- SymbolTable.java
+++ SymbolTable.java
@@ -32,7 +32,7 @@ class ArrayType extends Type {
   public Type baseType() { return baseType;}
 
   public String toString () {
-    return "array (" + dimension + ", " + baseType + ")";
+    return baseType.toString();
   }
 
 }
@@ -206,26 +206,52 @@ public class SymbolTable {
     enter (id, new SymbolTableEntry (Category . CLASS, env));
   }
   
-  //Adds code to the itermidiate code of env.
-  public void emit(String code){
-    intermediateCode += code + "\n";
+  //Adds the lineOfCode to the itermidiate code.
+  public void emit(String lineOfCode){
+    intermediateCode += "   " + lineOfCode + ";\n";
   }
   public void emitAssign(Expression e, Expression e2){
     String assignmentStatement;
     if(e.isArray)
-      assignmentStatement = e.addr +"["+ e.exp.addr +"]"+ " = " + e2.addr + ";";
+      assignmentStatement = e.addr +"["+ e.exp.addr +"]"+ " = " + e2.addr;
     else
-      assignmentStatement = e.addr + " = " + e2.addr + ";";
+      assignmentStatement = e.addr + " = " + e2.addr;
     
     emit(assignmentStatement);
   }
+
+  /*
+  * Adds a print statement to the intermidiate code.
+  */
+  public void emitPrintStatement(Expression e){
+    String printStatement = "print(\"%d\", "+ e.addr +")";
+
+    emit(printStatement);
+  }
+
+  /*
+  * Adds a scanf statement to the intermidiate code.
+  */
+  public void emitScanStatement(Expression e){
+    String scanStatement = "scanf(\"%d\", &"+ e.addr +")";
+    emit(scanStatement);
+
+  }
+
+  /*
+  * Adds an array assignment statement to the intermidiate code.
+  */
   public Expression emitArrayAssign(Expression e){
-    String arrayAssignment = tempVariable(numberOfTemps) + " = " + e.addr +"["+ e.exp.addr +"];";
+    String arrayAssignment = tempVariable(numberOfTemps) + " = " + e.addr +"["+ e.exp.addr +"]";
     emit(arrayAssignment);
     return new Expression(tempVariable(numberOfTemps), false);
   }
+
+  /*
+  * Adds a three address statement to the intermidiate code.
+  */
   public Expression emitThreeAddress(Expression e, Expression e2, String oper){
-    String threeAd = tempVariable(numberOfTemps) + " = "+ e.addr + oper + e2.addr + ";";
+    String threeAd = tempVariable(numberOfTemps) + " = "+ e.addr + oper + e2.addr;
     emit(threeAd);
     return new Expression(tempVariable(numberOfTemps), false);
   }
@@ -259,7 +285,9 @@ public class SymbolTable {
       Map . Entry <String, SymbolTableEntry> entry = envIterator . next ();
       String id = entry . getKey ();
       SymbolTableEntry idEntry = entry . getValue ();
-            
+      if (idEntry . category () == Category . VARIABLE){
+        printDeclaration(idEntry, id);
+      }    
       if (idEntry . category () == Category . FUNCTION){
        idEntry . localEnv () . printFunction (idEntry, id); 
       }
@@ -271,6 +299,7 @@ public class SymbolTable {
 
   public void printFunction(SymbolTableEntry funcOrClass, String funcName){
     System.out.print(funcOrClass.type() + " " + funcName + "{\n");
+    
     Iterator <Map . Entry <String, SymbolTableEntry>> envIterator = 
       table . entrySet () . iterator ();
    
@@ -278,29 +307,32 @@ public class SymbolTable {
       Map . Entry <String, SymbolTableEntry> entry = envIterator . next ();
       String id = entry . getKey ();
       SymbolTableEntry idEntry = entry . getValue ();
-      Type type = idEntry.type();
-      ArrayType aType;
-     if(type.getClass() == ArrayType.class){
-            aType = (ArrayType) type;
-            System.out.println(aType.baseType() + " " + id + "["+aType.dimension()+"]");
-     }else{
-      System . out . println (idEntry+" "+id + ";");
-     }
+      printDeclaration(idEntry, id);
     } 
-    int i = 1;
-    while(i < numberOfTemps){
-      System.out.println("int _T" + (i++) + ";");
-    }
+
+    printTempVariables();
     System.out.println(intermediateCode);
 
-      // if (idEntry . category () == Category . CLASS 
-      //     || idEntry . category () == Category . FUNCTION){
-      //  idEntry . localEnv () . printFunction (id); 
-        
-      // }
     System.out.println("}\n");
 
   }//printFunction
+
+  public void printDeclaration(SymbolTableEntry t, String id){
+    System . out . print("   ");
+    if(t.type().getClass() == ArrayType.class){
+      ArrayType aType = (ArrayType) t.type();
+      System.out.println(aType + " " + id + "["+aType.dimension()+"];");
+     }else{
+       System.out.println(t + " " + id + ";");
+     }
+  }
+
+  public void printTempVariables(){
+    int i = 1;
+    while(i < numberOfTemps){
+      System.out.println("   int"+ tempVariable(i++) + ";");
+    }
+  }
 
 }
 
--- tests/1test.cc
+++ tests/1test.cc
@@ -7,7 +7,11 @@ using namespace std;
 int main ()
 {
   int q; int r; int x; int y;
-  
+  q = r + 1;
+  r = x + q;
+
+  q = x + y;
+
   cout << q;
   return 0;
 }
